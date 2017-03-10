3a60e0d7167763560743e4f95e9c7aa9ceebe51e
Jianlucah <gianlucavarrazzo@gmail.com>
2017-03-10 16:24:02 +0100

    Update user.js

--- user.js
+++ user.js
@@ -7,23 +7,7 @@ user_pref("browser.fullscreen.animate", false);
 //Disabilitare la funzione prefetch//
 user_pref("network.prefetch-next", false);
 
-//Disabilitare segnaposto immagini mancanti
-user_pref("browser.display.show_image_placeholders", false);
-
-//Abilitare Pipelining
-user_pref("network.http.pipelining", true);
-user_pref("network.http.pipelining.maxrequests", 8);
-
-//Velocizzare delay caricamento pagine
-user_pref("nglayout.initialpaint.delay", 100);
-
 //OTTIMIZZARE(consigliate)//
-
-//Disabilitare la modalità protetta di Flash
-user_pref("dom.ipc.plugins.flash.disable-protected-mode", true);
-
-//Abilitare i video html5 720p/1080p
-user_pref("media.mediasource.webm.enabled", true);
 
 //Diminuire il consumo di RAM
 user_pref("browser.sessionhistory.max_total_viewers", 6);
@@ -43,13 +27,10 @@ user_pref("browser.slowStartup.notificationDisabled", true);
 user_pref("browser.slowStartup.maxSamples", 0);
 user_pref("browser.slowStartup.samples", 0);
 
-//PRIVACY (consigliate)//
+//Abilitare i video html5 720p/1080p
+user_pref("media.mediasource.webm.enabled", true);
 
-//Disabilitare il controllo dei siti
-user_pref("browser.safebrowsing.enabled", false);
-user_pref("browser.safebrowsing.malware.enabled", false);
-user_pref("services.sync.prefs.sync.browser.safebrowsing.enabled", false);
-user_pref("services.sync.prefs.sync.browser.safebrowsing.malware.enabled", false);
+//PRIVACY (consigliate)//
 
 //Connessioni speculative
 user_pref("network.http.speculative-parallel-limit", 0);
@@ -65,18 +46,6 @@ user_pref("signon.autofillForms.http", true);
 user_pref("media.peerconnection.enabled", false);
 
 //UTILITÀ (facoltative)//
-
-//Disabilitare auto completamento urlbar
-user_pref("browser.urlbar.autoFill", false);
-
-//Aprire i risultati della barra di ricerca in una nuova scheda
-user_pref("browser.search.openintab", true);
-
-//Decidere ogni volta se salvare la sessione delle schede
-user_pref("browser.showQuitWarning", true);
-
-//Impedire l'auto play dei video HTML5
-user_pref("media.autoplay.enabled", false);
 
 //Disabilitare Pocket
 user_pref("extensions.pocket.enabled", false);
