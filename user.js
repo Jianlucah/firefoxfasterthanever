753457820e222bfac6accbf67edc6cbdb879268f
Gianluca <jianlucah@elliot.local>
2017-04-18 12:12:47 +0200

    Add Fix Phishing Unicode Attack

--- user.js
+++ user.js
@@ -1,4 +1,4 @@
-//VELOCIZZARE (consigliate)//
+/*************** PRIVACY ***************/
 
 //Disabilitare tutte le animazioni
 user_pref("browser.tabs.animate", false);
@@ -7,16 +7,13 @@ user_pref("browser.fullscreen.animate", false);
 //Disabilitare la funzione prefetch//
 user_pref("network.prefetch-next", false);
 
-//OTTIMIZZARE(consigliate)//
+/*************** OTTIMIZZARE ***************/
 
 //Diminuire il consumo di RAM
 user_pref("browser.sessionhistory.max_total_viewers", 6);
 
 //Limitare il numero di pagine memorizzate nella cronologia
 user_pref("browser.sessionhistory.max_entries", 20);
-
-//Disabilitare IPV6
-user_pref("network.dns.disableIPv6", false);
 
 //Funzione di ripristino sessione
 user_pref("browser.sessionstore.interval", 300000);
@@ -30,24 +27,24 @@ user_pref("browser.slowStartup.samples", 0);
 //Abilitare i video html5 720p/1080p
 user_pref("media.mediasource.webm.enabled", true);
 
-//PRIVACY (consigliate)//
+/*************** PRIVACY ***************/
 
-//Connessioni speculative
+//Disabilitare Connessioni speculative
 user_pref("network.http.speculative-parallel-limit", 0);
 
-//Web beacons
+//Disabilitare Web beacons
 user_pref("beacon.enabled", false);
 
 //Disabilitare il messaggio di avvertimento su siti http non sicuri che chiedono login e password agli utenti
 user_pref("security.insecure_field_warning.contextual.enabled", false);
 user_pref("signon.autofillForms.http", true);
 
-//WebRTC
+//Disabilitare WebRTC
 user_pref("media.peerconnection.enabled", false);
 
-//UTILITÀ (facoltative)//
+/*************** UTILITA' ***************/
 
-//Disabilitare Pocket
+//DISABILITARE POCKET
 user_pref("extensions.pocket.enabled", false);
 
 //MULTIPROCESSO
@@ -55,9 +52,11 @@ user_pref("browser.tabs.remote.autostart", true);
 user_pref("browser.tabs.remote.autostart2", true);
 user_pref("extensions.e10sBlocksEnabling", false);
 
-//SCROLLING
+//FIX SCROLLING TREMOLANTE
 user_pref("general.smoothScroll.mouseWheel.durationMinMS", 250);
 user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 250);
 
-//SICUREZZA
-user_pref("webgl.disabled", true);
+/*************** SICUREZZA ***************/
+
+//Fix Attacco Phishing Unicode 
+user_pref("network.IDN_show_punycode", true);
